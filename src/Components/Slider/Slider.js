b76ccfe52e5cc95b824da9c9d54d2df4af61d445
unknown <montasirazad@gmail.com>
2023-02-20 02:22:55 +0600

    testing repo

---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import mountainImg from '../../image/img_mountains_wide.jpg';
+import natureImg from '../../image/img_nature_wide.jpg';
+import snowImg from '../../image/img_snow_wide.jpg';
+import './Slider.css';
+
+const Slider = () => {
+
+
+
+
+    return (
+
+        <div>
+            <h1>slider</h1>
+        </div>
+
+
+    );
+};
+
+export default Slider;
