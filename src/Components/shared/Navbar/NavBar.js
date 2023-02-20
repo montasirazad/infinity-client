d1aba147207f452ff7f9d6bb93efff8f0bfb5e9a
unknown <montasirazad@gmail.com>
2023-02-20 16:13:31 +0600

    installed mi ui package

--- src/Components/shared/Navbar/NavBar.js
+++ src/Components/shared/Navbar/NavBar.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import infinityLogo from '../../../image/infinity-logo.png';
 import './Navbar.css';
+import LoginIcon from '@mui/icons-material/Login';
 
 const Navbar = () => {
     return (
         <div className='header'>
 
             <div className='logo-div'>
-                <a href="">
+                <a href=" ">
                     <img src={infinityLogo} alt="" />
                 </a>
             </div>
 
             <nav >
 
-                <a href="">Home</a>
-                <a href="">Products</a>
-                <a href="">Retail</a>
-                <a href="">Manufacturer</a>
-                <a href="">Log in</a>
+                <a href=" ">Home</a>
+                <a href=" ">Products</a>
+                <a href=" ">Retail</a>
+                <a href=" ">Manufacturer</a>
+                <a href=" " ><LoginIcon style={{fontSize:'16px'}}/>Log in</a>
             </nav>
 
         </div>
