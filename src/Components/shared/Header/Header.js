9df6a5a5aaac864a14d05ea7b6d9bc906f5afcf3
unknown <montasirazad@gmail.com>
2023-02-21 02:36:13 +0600

    header edited

--- src/Components/shared/Header/Header.js
+++ src/Components/shared/Header/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
             <div className='my-3'>
                 <Navbar style={{ background: 'orange' }} expand="lg">
                     <Container>
-                        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+                       
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
